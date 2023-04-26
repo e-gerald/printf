baea64a4daff76df927724f1d52d89cd36957151
Gerald Enyenwa <geraldenyenwa@live.com>
2023-04-26 20:12:45 +0000

    Printf Function

--- main.h
+++ main.h
@@ -43,6 +43,8 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size);
 
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
